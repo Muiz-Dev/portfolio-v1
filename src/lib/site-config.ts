c829d5d07871543800a7ddd7bd11296a179c55f5
Muiz-Dev <muiz.dev.io@gmail.com>
2026-05-21 15:27:57 +0100

    feat: update site URL in configuration to new domain

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,4 +1,4 @@
-const rawSiteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://muizdev.com";
+const rawSiteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://solutions.muizdev.xyz";
 
 export const siteConfig = {
   name: "Muiz Dev Solutions",
